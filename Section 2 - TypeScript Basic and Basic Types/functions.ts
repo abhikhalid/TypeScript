63785a6f2ef2090199ebf0f4a7bbdfe533928a53
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 14:49:04 +0600

    The "never" type

--- Section 2 - TypeScript Basic and Basic Types/functions.ts
+++ Section 2 - TypeScript Basic and Basic Types/functions.ts
@@ -5,3 +5,16 @@ function add(a: number, b: number):number {
 function log(message: string): void {
     console.log(message);
 }
+
+//typescript infers the return type of function as void
+function logAndThrow(errorMessage: string) {
+    console.log(errorMessage);
+    throw new Error(errorMessage);
+}
+
+//but you could also explicitly specify the return type 'never' for functions that never return a value
+//you could say that the function never completes
+function logAndThrow(errorMessage: string): never {
+    console.log(errorMessage);
+    throw new Error(errorMessage);
+}
