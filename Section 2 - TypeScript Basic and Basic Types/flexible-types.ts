fdd31917017077934d30888c9bd91fe9cebce085
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 12:30:47 +0600

    The 'any' type

---
+++ Section 2 - TypeScript Basic and Basic Types/flexible-types.ts
@@ -0,0 +1,10 @@
+let age: any = 36;
+
+//......
+age = '37';
+age = false;
+age = {};
+age = [];
+
+//Don't use any type. Use specific types instead.
+// Because it defeats the purpose of TypeScript.
