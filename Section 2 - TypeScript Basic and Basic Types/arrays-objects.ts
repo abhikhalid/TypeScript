bb2a80303bc03665fa95dd2043f00215599db7ab
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 12:53:18 +0600

    A First Glimpse at Generic Types - Alternative Array Type Declaration

--- Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
+++ Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
@@ -2,8 +2,13 @@ let hobbies = ['Sports', 'Cooking'];
 
 // hobbies.push(10); // This will cause a type error
 
-let users: (string | number)[];
+// let users: (string | number)[];
+let users: Array<string | number>;
 
 users = [1, 'Max'];
+console.log('users:', users);
 users = [5, 1];
+console.log('users:', users);
 users = ['Max', 'Anna'];
+console.log('users:', users);
+console.log('hobbies:', hobbies);
