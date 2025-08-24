636c7272736555a0f3514858065a1bf9fe7dd908
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 17:59:05 +0600

    Forced "Not Null" and Optional Chaining

--- Section 2 - TypeScript Basic and Basic Types/form.ts
+++ Section 2 - TypeScript Basic and Basic Types/form.ts
@@ -1,9 +1,11 @@
+// const inputEl = document.getElementById('user-name')!;
+
 const inputEl = document.getElementById('user-name');
 
 // console.log(inputEl.value);
 
-if(!inputEl) {
-    throw new Error('Unable to find user-name element');
-}
+// if(!inputEl) {
+//     throw new Error('Unable to find user-name element');
+// }
 
-console.log(inputEl.value);
+console.log(inputEl?.value);
