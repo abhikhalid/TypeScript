aa2508e19fc24a0a0e3f0270969ae4d0144b7b83
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 12:57:57 +0600

    Making Sense of Tuples

--- Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
+++ Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
@@ -6,9 +6,16 @@ let hobbies = ['Sports', 'Cooking'];
 let users: Array<string | number>;
 
 users = [1, 'Max'];
-console.log('users:', users);
+// console.log('users:', users);
 users = [5, 1];
-console.log('users:', users);
+// console.log('users:', users);
 users = ['Max', 'Anna'];
-console.log('users:', users);
-console.log('hobbies:', hobbies);
+// console.log('users:', users);
+// console.log('hobbies:', hobbies);
+
+//Making Sense of Tuples
+// let possibleResults: number[]; // [1, -1]
+let possibleResults: [number, number]; // [1, -1]
+
+possibleResults = [1, -1];
+// possibleResults = [5,10,12]; // This will cause a type error
