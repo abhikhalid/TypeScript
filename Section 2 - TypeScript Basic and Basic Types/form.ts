97772e135874458780677ba14052db3b35061a10
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 17:51:40 +0600

    Inferred null & A First Look At Type Narrowing

---
+++ Section 2 - TypeScript Basic and Basic Types/form.ts
@@ -0,0 +1,9 @@
+const inputEl = document.getElementById('user-name');
+
+// console.log(inputEl.value);
+
+if(!inputEl) {
+    throw new Error('Unable to find user-name element');
+}
+
+console.log(inputEl.value);
