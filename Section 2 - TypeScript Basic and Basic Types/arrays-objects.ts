14b689f6ed3633dd89742d975040c6068d0adcfb
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 12:45:23 +0600

    Advanced Array Types

--- Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
+++ Section 2 - TypeScript Basic and Basic Types/arrays-objects.ts
@@ -2,3 +2,8 @@ let hobbies = ['Sports', 'Cooking'];
 
 // hobbies.push(10); // This will cause a type error
 
+let users: (string | number)[];
+
+users = [1, 'Max'];
+users = [5, 1];
+users = ['Max', 'Anna'];
