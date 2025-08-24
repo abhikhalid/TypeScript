a6f0f4b7e44aacff442a241480dff17560a001b9
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 17:36:18 +0600

    Functions as Types

--- Section 2 - TypeScript Basic and Basic Types/functions.ts
+++ Section 2 - TypeScript Basic and Basic Types/functions.ts
@@ -18,3 +18,28 @@ function logAndThrow(errorMessage: string): never {
     console.log(errorMessage);
     throw new Error(errorMessage);
 }
+
+
+const logMsg = (msg: string) => {
+    console.log(msg);
+}
+
+function performJob(cb: (msg: string) => void) {
+    cb("Job is done");  
+}
+
+performJob(logMsg);
+
+type User = {
+    name: string;
+    age: number;
+    greet: () => string;
+};
+
+let user: User = {
+    name: 'Max',
+    age: 32,
+    greet() {
+        return 'Hello, I am ' + this.name;
+    }
+}
