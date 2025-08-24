aebabe08540d01674234a01e298a11793ec8ecbd
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-24 12:16:41 +0600

    Type Inference vs Type Assignment

--- Section 2 - TypeScript Basic and Basic Types/basics.ts
+++ Section 2 - TypeScript Basic and Basic Types/basics.ts
@@ -1,4 +1,8 @@
 // let userName;
 let userName : string;
+let userAge = 38; //Type infere the type as number
+let useAge : number;
 
+//...
 userName = 'Max';
+useAge = '34'; // Error: Type 'string' is not assignable to type 'number'.
