09140e3e437693ec1204721d67362fed645bf4d1
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-08-30 12:42:59 +0600

    Section 4: TypeScript Essentials Demo Project

---
+++ Section 4 -  TypeScript Essentials Demo Project/calculator.ts
@@ -0,0 +1,15 @@
+//data:
+//initial amount
+//annual contribution
+//expected return
+//duration
+
+function calculateInvestment(data) { } //result[]
+
+function printResults(result) {
+    //print (output) the result data
+}
+
+const results = calculateInvestment(...)
+
+printResults(results);
