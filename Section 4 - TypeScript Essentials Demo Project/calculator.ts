51462e0ebb15f792ac5f99f7efcff39f6f587ca0
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-02 10:32:59 +0600

    Getting Started & First Steps

---
+++ Section 4 - TypeScript Essentials Demo Project/calculator.ts
@@ -0,0 +1,14 @@
+//data:
+// initial amount
+// annual contribution
+// expected annual return
+// duration
+
+function calculateInvestment(data) {} //  => result[]
+
+function printResults(results) {} // => print (output) the result data
+
+const results =calculateInvestment(...)
+
+
+printResults(results)
