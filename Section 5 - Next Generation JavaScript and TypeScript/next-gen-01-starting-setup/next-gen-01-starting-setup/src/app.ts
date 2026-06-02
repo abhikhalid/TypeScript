ca963bc00d86e8babde42e77de4f5a28269ab3b1
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-02 13:46:42 +0600

    Arrow Funcions

--- Section 5 - Next Generation JavaScript and TypeScript/next-gen-01-starting-setup/next-gen-01-starting-setup/src/app.ts
+++ Section 5 - Next Generation JavaScript and TypeScript/next-gen-01-starting-setup/next-gen-01-starting-setup/src/app.ts
@@ -1,18 +1,34 @@
-const userName = 'Khalid'
+// const userName = 'Khalid'
 
-let age = 30;
+// let age = 30;
 
-age = 29;
+// age = 29;
 
-function add(a: number, b: number): number {
- let result = a + b; // result is a local variable that holds the sum of a and b (only available within the function)
- return result;
+// function add(a: number, b: number): number {
+//  let result = a + b; // result is a local variable that holds the sum of a and b (only available within the function)
+//  return result;
+// }
+
+// console.log(add(5, 10));
+
+
+
+// if(age > 18) {
+//  console.log('You are an adult.');
+// }
+
+const add = (a: number, b: number): number => {
+    return a + b;
 }
 
-console.log(add(5, 10));
+
+const add2 = (a: number, b: number): number => a + b;
 
 
+const printOutput: (output: string | number) => void = output => console.log(output);
 
-if(age > 18) {
- console.log('You are an adult.');
+const button = document.querySelector('button')!;
+
+if(button){
+    button.addEventListener('click', event => console.log(event));
 }
