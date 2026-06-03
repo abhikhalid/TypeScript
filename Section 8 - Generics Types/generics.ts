ef444f34f1a67c40684c1f70ca4d2730b0a73773
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-03 16:46:41 +0600

    Working wiht Generic Classes & Interfaces

--- Section 8 - Generics Types/generics.ts
+++ Section 8 - Generics Types/generics.ts
@@ -19,3 +19,18 @@ function merge<T extends object, U extends object>(a: T, b: U) {
 
 const merged = merge({id: 1}, {name: 'Khalid'});
 console.log(merged);
+
+
+interface Role<T>{
+    
+}
+
+class User<T>{
+    constructor(public id: T) {
+    }
+}
+
+const user1 = new User<number>(1);
+const user2 = new User<string>('abc');
+user1.id; // number
+user2.id; // string
