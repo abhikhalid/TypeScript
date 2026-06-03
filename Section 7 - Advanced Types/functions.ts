0ea2ced2905ee727155c718c14d32ef488913970
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-03 12:49:55 +0600

    Function Overloads

---
+++ Section 7 - Advanced Types/functions.ts
@@ -0,0 +1,11 @@
+function getLength(val: string | any[]) {
+  if(typeof val === 'string'){
+    const numberOfWords = val.split(' ').length;
+    return `${numberOfWords} words`;
+  }
+  return val.length;
+}
+
+const numOfWords = getLength('does this work? ');
+const numItems = getLength(['Sports','Cookies']);
+
