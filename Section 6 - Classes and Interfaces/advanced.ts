93e1b96c6ee064d5bcb79a22936dc5dffb533a4d
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-03 09:07:01 +0600

    Setting Values with Setters

--- Section 6 - Classes and Interfaces/advanced.ts
+++ Section 6 - Classes and Interfaces/advanced.ts
@@ -5,15 +5,36 @@
 
 
 class User{
-    constructor(private firstName: string, private lastName: string) {}
+    private _firstName: string = '';
+    private _lastName: string = '';
 
     // This is a getter for the fullName property
     get fullName(): string {
-        return `${this.firstName} ${this.lastName}`;
+        return `${this._firstName} ${this.lastName}`;
+    }
+
+    set firstName(name: string) {
+        if(name.trim() === '') {
+            throw new Error('First name cannot be empty');
+        }
+
+        this._firstName = name;
+    }
+
+      set lastName(name: string) {
+        if(name.trim() === '') {
+            throw new Error('Last name cannot be empty');
+        }
+
+        this._lastName = name;
     }
 }
 
-const user = new User("John", "Doe");
+
+const user = new User();
+user.firstName = 'John';
+user.lastName = 'Doe';
 console.log(user.fullName); // Output: John Doe
 
-// type 'tsc' and 'node advanced.js' in the terminal to compile and run the code respectively.
+// In this example, we have a `User` class with private properties `_firstName` and `_lastName`. We define a getter `fullName` that returns the full name of the user by concatenating the first and last names. We also have setter methods for `firstName` and `lastName` to allow setting these properties while ensuring they are not empty. When we create an instance of the `User` class and set the first and last names, we can access the full name using the `fullName` getter.
+
