d8d4bad0cb2c73afcb813cee20533638bfbf0090
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-03 12:38:39 +0600

    Type Guards via "instanceof"

---
+++ Section 7 - Advanced Types/guards-updated.ts
@@ -0,0 +1,54 @@
+type FileSource = { type: 'file'; path: string };
+const fileSource: FileSource = {
+  type: 'file',
+  path: 'some/path/to/file.csv',
+};
+
+type DBSource = { type: 'db', connectionUrl: string };
+const dbSource: DBSource = {
+  type: 'db',
+  connectionUrl: 'some-connection-url',
+};
+
+type Source = FileSource | DBSource;
+
+function loadData(source: Source) {
+  // if ('path' in source) {
+  if (source.type === 'file') {
+    // source.path
+    // source.path; => use that to open the file
+    return;
+  }
+  // source.connectionUrl; => to reach out to database
+}
+
+class User {
+  constructor(public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor(permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+  if(entity instanceof User) {
+    // entity.name; => use that to greet the user
+    // entity.join(); => use that to let the user join
+    return;
+  }
+
+  // entity.scan(); => use that to let the admin scan for issues
+}
