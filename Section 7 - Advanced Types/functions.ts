b6362e85afc55dd16c1189a30d314088d238d9d0
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-03 13:02:03 +0600

    Working with Function Overloads

--- Section 7 - Advanced Types/functions.ts
+++ Section 7 - Advanced Types/functions.ts
@@ -1,9 +1,12 @@
+function getLength(val: any[]): number;
+function getLength(val: string): string;
+
 function getLength(val: string | any[]) {
-  if(typeof val === 'string'){
-    const numberOfWords = val.split(' ').length;
-    return `${numberOfWords} words`;
-  }
-  return val.length;
+    if (typeof val === 'string') {
+        const numberOfWords = val.split(' ').length;
+        return `${numberOfWords} words`;
+    }
+    return val.length;
 }
 
 const numOfWords = getLength('does this work? ');
