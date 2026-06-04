4ac7215af48e6b3d98c7dde2c1b7d0253193ae2d
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-04 13:02:15 +0600

    "typeof" & A More Useful Example

--- Section 10 - Deriving Types From Types/typeof.ts
+++ Section 10 - Deriving Types From Types/typeof.ts
@@ -8,3 +8,19 @@ console.log(typeof userName);
 
 type UserName = typeof userName;
 
+
+
+const settings = {
+    difficulty: 'easy',
+    minLevel: 10,
+    didStart: false,
+    players: ['John','Jane']
+};
+
+type Settings = typeof settings;
+
+function loadData(settings: Settings){
+    //...
+}
+
+loadData(settings);
