90d0d31a0bc99f3fdb03e2602789e4c378dded55
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-04 12:51:18 +0600

    Using "typeof"

---
+++ Section 10 - Deriving Types From Types/typeof.ts
@@ -0,0 +1,10 @@
+// to run in the temrinal. Follow these steps
+// 'tsc' to compiler
+// node typeof.js
+
+let userName = 'Max';
+
+console.log(typeof userName);
+
+type UserName = typeof userName;
+
