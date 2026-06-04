baa8f5f41f3a715cd58f8cb2a5b69c326e4945ec
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-04 12:13:41 +0600

    Adding an "add' Method

--- Section 9 - Classes & Generics - Demo Project/linked-list.ts
+++ Section 9 - Classes & Generics - Demo Project/linked-list.ts
@@ -1,11 +1,25 @@
-class ListNode{}
+class ListNode<T>{
+    constructor(public value: T, public next?: ListNode<T>){
+        
+    }
+}
 
 class LinkedList<T>{
-    private root?: ListNode;
+    private root?: ListNode<T>;
     private length = 0;
 
     add(value: T){
-
+        const node = new ListNode(value);
+        if(!this.root){
+            this.root = node;
+        } else {
+            let current = this.root;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = node;
+        }
+        this.length++;
     }
 }
 
