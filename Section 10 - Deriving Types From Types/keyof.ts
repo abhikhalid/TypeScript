f019810007f7206e087edc21f803f30875a210af
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-04 14:13:32 +0600

    "keyof"  & A More useful Example

--- Section 10 - Deriving Types From Types/keyof.ts
+++ Section 10 - Deriving Types From Types/keyof.ts
@@ -16,3 +16,23 @@ let validKey: UserKeys;
 
 validKey = 'name';
 validKey = 'age';
+// validKey = 'sdfde'; //error
+
+function getProp<T extends object,U extends keyof T>(obj:T, key:U){
+    const val = obj[key];
+
+    if(val === undefined || val==null) {
+        throw new Error("Accessing undefined or null value");
+    }
+
+    return val;
+}
+
+const user = {name: 'Khalid', age: 30};
+
+const val = getProp(user,'age');
+
+const data = {id:1, isStored:false,values:[1,-5,10]};
+
+const isStored = getProp(data,'isStored');
+
