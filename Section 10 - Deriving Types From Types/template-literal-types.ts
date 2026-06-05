768eedfd884907130a9b828ff66c3f71dbd6f680
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-05 12:13:10 +0600

    Exploring Template Literal Types

---
+++ Section 10 - Deriving Types From Types/template-literal-types.ts
@@ -0,0 +1,28 @@
+// const mainUserName = 'Khalid';
+
+// const greeting = `Hi there, ${mainUserName}`;
+
+
+type ReadPermissions = 'no-read' | 'read';
+type WritePermissions = 'no-write' | 'write';
+
+// type FilePermissions = 'no-read-write'  
+// |'read-no-write' 
+// | 'no-read-no-write'
+// | 'read-write';
+
+// instead, I want to have new string literals
+
+type FilePermissions = `${ReadPermissions}-${WritePermissions}`;
+
+
+type DataFile = {
+    data: string;
+    permissions: FilePermissions;
+};
+
+type DataFileEventNames = `${keyof DataFile}Changed`;
+
+type DataFileEvents = {
+    [Key in DataFileEventNames] : ()=> void;
+};
