d000497573a72fa298a4ba948b318ec820f9fd4f
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-05 17:00:43 +0600

    Conditional Types - Another Example

--- Section 10 - Deriving Types From Types/conditional.ts
+++ Section 10 - Deriving Types From Types/conditional.ts
@@ -10,3 +10,21 @@ let text = 1;
 type GetElementType<T> = T extends any[] ? T[number] : never;
 type Example2 = GetElementType<StringArray>;
 type Example3 = GetElementType<typeof text>;
+
+
+type FullnamePerson = {firstName: string; lastName: string;};
+type FullnameOrNothing<T> = T extends FullnamePerson ? string : never;
+
+function getFullname<T extends object>(person: T): FullnameOrNothing<T>{
+    if('firstName' in person &&
+         'lastName' in person && 
+         person.firstName &&
+          person.lastName){
+            return `${person.firstName} ${person.lastName}` as FullnameOrNothing<T>;
+    }
+
+    throw new Error('No first name and / or last name found');
+}
+
+const name1 = getFullname({});
+const name2 = getFullname({firstName:'Khalid',lastName:'Mahmud'});
