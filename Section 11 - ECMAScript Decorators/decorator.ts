c86a13b6a445dac067b6ef881642b50fbd75fb10
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-08 16:49:38 +0600

    Building a Class Decorators that Edits a class

--- Section 11 - ECMAScript Decorators/decorator.ts
+++ Section 11 - ECMAScript Decorators/decorator.ts
@@ -1,9 +1,13 @@
 // Decorator is an object oriented related feature, I will start by adding a class
 
-function logger(target: any, ctx: ClassDecoratorContext){
+function logger<T extends new (...args: any[]) => any>(target: T, ctx: ClassDecoratorContext){
     console.log('logger decorator');
     console.log(target);
     console.log(ctx);
+
+    return class extends target{
+        age = 30;
+    }
 }
 
 @logger
@@ -14,6 +18,9 @@ class Person{
         console.log('Hi, I am '+ this.name);
     }
 }
+
+const person = new Person();
+console.log(person);
 
 //in the terminal
 // tsc
