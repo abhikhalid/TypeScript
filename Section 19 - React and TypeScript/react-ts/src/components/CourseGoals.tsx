1a6a5342b9d221e774724e55647e3be83e535f21
khalid.mahmud <khalid.mahmud@bjitgroup.com>
2026-06-12 12:54:55 +0600

    Component Functions & Types - An Alternative

--- Section 19 - React and TypeScript/react-ts/src/components/CourseGoals.tsx
+++ Section 19 - React and TypeScript/react-ts/src/components/CourseGoals.tsx
@@ -11,7 +11,8 @@ interface CourseGoalsProps {
   goals: Goal[];
 };
 
-export default function CourseGoals({ goals }: CourseGoalsProps) {
+// export default function CourseGoals({ goals }: CourseGoalsProps) { // modern approach
+const CourseGoals: React.FC<CourseGoalsProps> = ({ goals }) => { //old approach
   return (
     <ul>
         {goals.map((goal) => (
